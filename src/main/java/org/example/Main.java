c085de593df40c0a3f22bcc767ed138339e061e4
KlaudiuszKudla <86147960+KlaudiuszKudla@users.noreply.github.com>
2024-06-12 21:23:31 +0200

    first version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,8 @@ import org.example.generators.JsonGenerator;
 import org.example.solver.ObjectFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,16 +23,35 @@ public class Main {
 
         int numberOfProducts = 500;
         Map<String, List<String>> products = JsonGenerator.generateProductsWithDelivery(numberOfProducts);
-        JsonGenerator.generateProductsWithDelivertJson(products, "src/main/resources/products.json");
+        Map<String, List<String>> products2 = new HashMap<>();
+        products2.put("Product 1", Arrays.asList("Courier", "In-store pick-up", "Parcel locker", "Pallet delivery", "Contactless delivery"));
+        products2.put("Product 2", Arrays.asList("Courier", "Overnight shipping", "Regional shipping", "Green delivery"));
+        products2.put("Product 3", Arrays.asList("Parcel locker", "In-store pick-up", "Temperature-controlled delivery","Standard post"));
+        products2.put("Product 4", Arrays.asList("In-store pick-up", "White glove service", "Weekend delivery"));
+        products2.put("Product 5", Arrays.asList("Courier", "Pick-up point", "Bulk shipping", "Standard post"));
 
-        int numberOfProducts2 = 20;
+
+
+//
+//        "Pick-up point", "Parcel locker", "Courier", "Same day delivery",
+//                "Next day shipping", "Mailbox delivery", "In-store pick-up", "Express Collection",
+//                "Drone delivery", "Bike courier", "Standard post", "Weekend delivery",
+//                "International shipping", "Economy delivery", "Scheduled delivery",
+//                "Evening delivery", "Green delivery", "Contactless delivery",
+//                "Temperature-controlled delivery", "Heavy item delivery", "Pallet delivery",
+//                "White glove service", "Overnight shipping", "Regional shipping",
+//                "Bulk shipping"
+
+        JsonGenerator.generateProductsWithDelivertJson(products2, "src/main/resources/products.json");
+
+        int numberOfProducts2 = 6;
         List<String> basket = JsonGenerator.generateProducts(numberOfProducts2);
         JsonGenerator.generateJsonWithProducts(basket, "src/main/resources/basket.json");
-//        ObjectFactory objectFactory = new ObjectFactory();
-//        System.out.println("Zachłanny: " + objectFactory.split(basket));
+        ObjectFactory objectFactory = new ObjectFactory();
+        System.out.println("Zachłanny: " + objectFactory.split(basket));
 
         GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
-        geneticAlgorithm.geneticAlgorithm(120, 1000, 80,10,8, "src/main/resources/genetic");
+        geneticAlgorithm.geneticAlgorithm(50, 1000, 80,10,8, "src/main/resources/genetic");
 
         GreedyAlgorithm greedyAlgorithm = new GreedyAlgorithm();
         greedyAlgorithm.greedyAlgorithm("src/main/resources/greedy");
